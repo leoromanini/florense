a830b403423937c20d0dd27b4d9163c5f935b558
Leonardo Romanini <leo.hromanini@gmail.com>
2020-07-01 22:41:03 -0300

    Implementado carrousel na tela de pedido

--- florense/portal/static/portal/js/order.js
+++ florense/portal/static/portal/js/order.js
@@ -2,3 +2,5 @@ $('.custom-file-input').on('change',function(){
                 var fileName = $(this).val();
                 $(this).next('.custom-file-label').html(fileName);
 })
+
+$('.carousel').carousel('pause');
